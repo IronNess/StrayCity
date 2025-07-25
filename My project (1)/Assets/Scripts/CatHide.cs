baccfd6a64eed8b51c640e55a28f25a0c312f5e3
Ness <146290658+IronNess@users.noreply.github.com>
2025-07-26 00:43:11 +0100

    Update CatHide.cs
    Script created for cat hiding in objects using bool. Referencing controllers and Triggers

--- My project (1)/Assets/Scripts/CatHide.cs
+++ My project (1)/Assets/Scripts/CatHide.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class CatHide : MonoBehaviour
 {
+    public GameObject zzzSymbol;
+    public float hideDuration = 3f;
+
+    private bool canHide = false;
+    private bool isHiding = false;
+
+    private Renderer[] catRenderers;
+    private CharacterController controller;
+
+    
     // Start is called before the first frame update
     void Start()
     {
+        catRenderers = GetComponentsInChildren<Renderer>();
+        controller = GetComponent<CharacterController>();
+
+        if (zzzSybol !=null)
+        zzzSybol.SetActive(false);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canHide && !isHiding && Input.GetKeyDown(KeyCode.H))
+        {
+            StartCoroutine(HideCat());
+        }
+    }
+
+    private IEnumerator HideCat()
+    {
+        isHiding = true;
+
+        foreach (Renderer r in catRenderers)
+        r.enabled = false;
+
+        if (controller != null)
+        controller.enabled = false;
+
+        if (zzzSymbol != null)
+        zzzSymbol.SetActive(true);
+
+        yield return new WaitForSeconds(hideDuration);
+
+        foreach (Renderer r in catRenderers)
+        r.enabled = true;
+
+        if (Controller != null)
+        controller.enabled = true;
+
+        if (zzzSymbol !+ null)
+        zzzSymbol.SetActive(false);
+
+        isHiding = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HideSpot"))
+        {
+            canHide = true;
+        }
+    }
+
+    void OnTriggerExit(collider other)
+    {
+        if (other.CompareTag("HideSpot"))
+        {
+            canHide = false;
+        }
     }
 }
