336dc36130523d993971ee4a354a109e2a77a81b
Ness <146290658+IronNess@users.noreply.github.com>
2025-07-25 21:06:14 +0100

    Update TrashCanTrigger.cs
    Created script for trashcan interaction. Currently not working in play mode so need to add a debug message and read from console

--- My project (1)/Assets/Scripts/TrashCanTrigger.cs
+++ My project (1)/Assets/Scripts/TrashCanTrigger.cs
@@ -1,18 +1,74 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TrashCanTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject fish2;
+    public Transform lid;
+    public float lidOpenAngle = -120f;
+    public float lidOpenSpeed = 5f;
+    public float lidCloseDelay = 2f;
+
+    private bool hasPopped = false;
+    private bool playerNear = false;
+    private Quaternion originalLidRotation;
+
     void Start()
     {
-        
+        if (lid != null)
+            originalLidRotation = lid.localRotation;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (playerNear && !hasPopped && Input.GetKeyDown(KeyCode.E))
+        {
+            hasPopped = true;
+
+            if (fish2 != null)
+                fish2.SetActive(true);
+
+            if (lid != null)
+                StartCoroutine(OpenAndCloseLid());
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNear = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNear = false;
+        }
+    }
+
+    private IEnumerator OpenAndCloseLid()
+    {
+        Quaternion openRotation = Quaternion.Euler(0, 0, lidOpenAngle);
+        float t = 0;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * lidOpenSpeed;
+            lid.localRotation = Quaternion.Slerp(originalLidRotation, openRotation, t);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(lidCloseDelay);
+
+        t = 0;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * lidOpenSpeed;
+            lid.localRotation = Quaternion.Slerp(openRotation, originalLidRotation, t);
+            yield return null;
+        }
     }
 }
