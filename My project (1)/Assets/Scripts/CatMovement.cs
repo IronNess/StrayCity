ea247cf5f2a430691d15a0355ed802a9d44ebced
Ness <146290658+IronNess@users.noreply.github.com>
2025-07-26 19:54:31 +0100

    Update CatMovement.cs

--- My project (1)/Assets/Scripts/CatMovement.cs
+++ My project (1)/Assets/Scripts/CatMovement.cs
@@ -5,61 +5,72 @@ using UnityEngine;
 public class CatMovement : MonoBehaviour
 {
     public float moveSpeed = 40f;
-    public float rotationSpeed = 200f; // How fast the Cat will turn direction
-    private Animator animator; // referencing animator for playing animations
-    private CharacterController controller; // Character Controller for movement
+    public float rotationSpeed = 200f;
+
+    private Animator animator;
+    private CharacterController controller;
 
     private bool isNearItem = false;
-    private GameObject currentItem; // For pickup of items
+    private GameObject currentItem;
 
+    private bool nearEnemy = false;
+    private GameObject currentEnemy;
 
-    // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>(); // aniamtor attached to my CatObject
-        controller = GetComponent<CharacterController>(); // Get character controller
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
 
-        if (animator != null) animator.applyRootMotion = false; 
-        
+        if (animator != null)
+            animator.applyRootMotion = false;
     }
 
-
-    // Update is called once per frame
     void Update()
     {
-        // Keyboard input
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 moveDir = (transform.right * h + transform.forward * v).normalized; // combine into movement direction vector (X, Y Z)
+        Vector3 moveDir = (transform.right * h + transform.forward * v).normalized;
+        controller.Move(moveDir * moveSpeed * Time.deltaTime);
 
-        controller.Move(moveDir * moveSpeed * Time.deltaTime); // Move cat using character controller
-
-        if (moveDir.sqrMagnitude > 0.0001f) // If movement, rotate cat in correct direction
+        if (moveDir.sqrMagnitude > 0.0001f)
         {
-
             Quaternion targetRot = Quaternion.LookRotation(moveDir, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotationSpeed * Time.deltaTime);
-             // This will rotate smoothly toward the direction the cat is walking
         }
 
+        if (animator != null)
+            animator.SetBool("isWalking", moveDir.sqrMagnitude > 0.0001f);
 
-            //Trigger the walking animation
-            if (animator != null) animator.SetBool("isWalking", moveDir.sqrMagnitude > 0.0001f);
-
-            if (isNearItem && Input.GetKeyDown(KeyCode.E))
-            {
-                if (animator != null)
+        // E to collect fish
+        if (isNearItem && Input.GetKeyDown(KeyCode.E))
+        {
+            if (animator != null)
                 animator.SetTrigger("CollectItem");
 
-                if (currentItem != null)
-                {
-                    Destroy(currentItem);
-                    Debug.Log("Item Collected");
-                }
-
-                isNearItem = false;
+            if (currentItem != null)
+            {
+                Destroy(currentItem);
+                Debug.Log("Item Collected");
             }
+
+            isNearItem = false;
+        }
+
+        // F to attack enemy (rat)
+        if (nearEnemy && Input.GetKeyDown(KeyCode.F))
+        {
+            if (animator != null)
+                animator.SetTrigger("CollectItem"); // reuse animation
+
+            if (currentEnemy != null)
+            {
+                Destroy(currentEnemy);
+                Debug.Log("Enemy defeated!");
+            }
+
+            nearEnemy = false;
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -69,14 +80,29 @@ public class CatMovement : MonoBehaviour
             isNearItem = true;
             currentItem = other.gameObject;
         }
+
+        if (other.CompareTag("Enemy"))
+        {
+            nearEnemy = true;
+            currentEnemy = other.gameObject;
+        }
     }
 
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Collectible"))
         {
-            isNearItem = false;
-            currentItem = null;
+            isNearItem = true;
+            currentItem = other.gameObject;
+        }
+
+        else if (other.CompareTag("Enemy"))
+        {
+           if (animator != null)
+           animator.SetTrigger("CollectItem");
+
+           Destroy(other.gameObject);
+           Debug.Log("Rat Caught");
         }
     }
 }
