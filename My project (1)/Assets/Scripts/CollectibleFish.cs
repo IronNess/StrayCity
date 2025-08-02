e2cb1e8e8d548325bb6a2ee5ca2f4e3e57d41c5b
Ness <146290658+IronNess@users.noreply.github.com>
2025-08-02 23:32:35 +0100

    Update CollectibleFish.cs

--- My project (1)/Assets/Scripts/CollectibleFish.cs
+++ My project (1)/Assets/Scripts/CollectibleFish.cs
@@ -6,6 +6,7 @@ public class CollectibleFish : MonoBehaviour, IInteractable
     // Called by CatMovement when the player presses E
     public void Interact(CatMovement cat)
     {
+        AudioManager.Instance.PlayCatMeow();
         GameManager.Instance.CollectFish();   // update HUD & game logic
         Destroy(gameObject);                  // remove the fish
     }
