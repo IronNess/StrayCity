6a284c7b2978593dcfc340d745b284125bd82aed
Ness <146290658+IronNess@users.noreply.github.com>
2025-08-02 11:25:58 +0100

    Create GameManager.cs
    Added a game manager script to control the cat collection, tutorial and end game conditions

---
+++ My project (1)/Assets/Scripts/GameManager.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameState { Start, Playing, Paused, GameOver }
+
+public class GameManager : MonoBehaviour
+
+{
+    public static GameManager Instance;
+
+    [Header("Panels")]
+    public GameObject startPanel;
+    public GameObject gamePanel;
+    public GameObject pausePanel;
+    public GameObject gameOverPanel;
+
+    [Header("HUD Icons")]
+    public GameObject[] fishIcons;
+    public GameObject ratIcon;
+
+    private int fishCollected = 0;
+    private bool ratCaught = false;
+
+    [Header("Game Completion Settings")]
+    public int totalFish = 3;
+
+    [Header("Tutorial")]
+    public GameObject tutorial;
+
+    public GameState CurrentState { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    void Start() 
+    {
+        if (tutorial != null) tutorial.SetActive(false);
+        ChangeState(GameState.Start);
+        
+    }
+
+    void Update()
+    {
+        if (CurrentState == GameState.Playing && Input.GetKeyDown(KeyCode.Escape))
+            ChangeState(GameState.Paused);
+        else if (CurrentState == GameState.Paused && Input.GetKeyDown(KeyCode.Escape))
+            ChangeState(GameState.Playing);
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        CurrentState = newState;
+
+        startPanel.SetActive(newState == GameState.Start);
+        gamePanel.SetActive(newState == GameState.Playing);
+        pausePanel.SetActive(newState == GameState.Paused);
+        gameOverPanel.SetActive(newState == GameState.GameOver);
+
+        Time.timeScale = (newState == GameState.Paused || newState == GameState.GameOver) ? 0f : 1f;
+    }
+
+    public void StartGame()
+    {
+     ChangeState(GameState.Playing);
+     if (tutorial != null)
+     tutorial.SetActive(true);
+    }
+
+
+    public void ResumeGame() => ChangeState(GameState.Playing);
+    public void Restart()    => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    public void Quit()       => Application.Quit();
+    public void GameOver()   => ChangeState(GameState.GameOver);
+
+  public void CollectFish()
+{
+    if (fishCollected >= fishIcons.Length) return;
+
+    fishIcons[fishCollected].SetActive(false);
+    fishCollected++;
+
+    CheckEndGameCondition();
+}
+    
+    public void CatchRat()
+    { 
+        ratIcon.SetActive(false);
+        ratCaught = true;
+        CheckEndGameCondition();
+}
+
+public void CloseTutorial()
+{
+    tutorial.SetActive(false);
+}
+
+
+private void CheckEndGameCondition()
+{
+    if (fishCollected == totalFish && ratCaught)
+    {
+        GameOver();
+    }
+}
+}
+
+
